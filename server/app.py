f130fb2fee461933be1aadac5feb6e56b691efeb
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-02-28 21:01:14 -0600

    Start lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    lines = [f"{number}\n" for number in range(parameter)]
+    return lines
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
