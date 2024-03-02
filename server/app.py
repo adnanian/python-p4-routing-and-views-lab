a4b814ca12bd9115ee8a50f16992d86371457e34
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-03-01 19:43:04 -0600

    Complete lab

--- server/app.py
+++ server/app.py
@@ -15,8 +15,25 @@ def print_string(parameter):
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    lines = [f"{number}\n" for number in range(parameter)]
-    return lines
+    counter = f""
+    for number in range(parameter):
+        counter += f"{number}\n" 
+    return counter
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        raise ValueError("Invalid operand!")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
